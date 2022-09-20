b61c14d50f7e52351cec4ce0831047580957b849
AnthonyRom12 <106614976+AnthonyRom12@users.noreply.github.com>
2022-09-20 14:10:55 +0300

    Initial commit

---
+++ item/item/urls.py
@@ -0,0 +1,35 @@
+"""item URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from products.views import (
+    CreateCheckOutSessionView,
+    ProductLandingPageView,
+    SuccessView,
+    CancelView,
+    stripe_webhook,
+    StripeIntentView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
+    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('', ProductLandingPageView.as_view(), name='landing-page'),
+    path('create-checkout-session/<pk>/', CreateCheckOutSessionView.as_view(), name='create-checkout-session')
+]
